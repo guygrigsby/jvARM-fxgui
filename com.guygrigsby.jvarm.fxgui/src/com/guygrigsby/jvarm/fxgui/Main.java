4456d6e27dab1ed3a483e155fabcd5227fbecb03
Guy <guy.grigsby@gmail.com>
2015-01-21 17:46:45 -0700

    Added javafx "complex" sample gui for starting point

--- com.guygrigsby.jvarm.fxgui/src/com/guygrigsby/jvarm/fxgui/Main.java
+++ com.guygrigsby.jvarm.fxgui/src/com/guygrigsby/jvarm/fxgui/Main.java
@@ -1,17 +1,17 @@
 package com.guygrigsby.jvarm.fxgui;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			GridPane root = (GridPane)FXMLLoader.load(getClass().getResource("Main.fxml"));
+			VBox root = (VBox)FXMLLoader.load(getClass().getResource("Main.fxml"));
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
