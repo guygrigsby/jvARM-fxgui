bc61a81c0c3eb9f33dd522776e792ad77b611c5d
Guy Grigsby <guy.grigsby@gmail.com>
2015-02-11 22:30:30 -0700

    added "open" menu action

--- com.guygrigsby.jvarm.fxgui/src/com/guygrigsby/jvarm/fxgui/Controller.java
+++ com.guygrigsby.jvarm.fxgui/src/com/guygrigsby/jvarm/fxgui/Controller.java
@@ -1,7 +1,10 @@
 package com.guygrigsby.jvarm.fxgui;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
@@ -17,6 +20,8 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TitledPane;
 import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import javafx.util.converter.IntegerStringConverter;
 
 import org.apache.logging.log4j.LogManager;
@@ -25,73 +30,106 @@ import org.apache.logging.log4j.Logger;
 import com.guygrigsby.jvarm.core.ArmProgram;
 import com.guygrigsby.jvarm.core.ArmSourceCompiler;
 
-
-
 public class Controller {
-	
+
 	private static Logger logger = LogManager.getLogger();
-	
-	@FXML private TextArea editor;
-	
-	@FXML private TableView<ObservableMap.Entry<String, Integer>> registersTable;
-	
-	@FXML private TableColumn<ObservableMap.Entry<String, Integer>,String> registerNameCol;
-	@FXML private TableColumn<ObservableMap.Entry<String, Integer>,Integer> registerValueCol;
-	@FXML private TitledPane registerPane;
-	@FXML private Accordion rightAccordion;
-	
+
+	@FXML
+	private TextArea editor;
+
+	@FXML
+	private TableView<ObservableMap.Entry<String, Integer>> registersTable;
+
+	@FXML
+	private TableColumn<ObservableMap.Entry<String, Integer>, String> registerNameCol;
+	@FXML
+	private TableColumn<ObservableMap.Entry<String, Integer>, Integer> registerValueCol;
+	@FXML
+	private TitledPane registerPane;
+	@FXML
+	private Accordion rightAccordion;
+
 	private ArmProgram program;
-	
+
 	private ObservableMap<String, Integer> registers;
-	
+
+	private Stage stage;
+
 	@FXML
 	private void initialize() {
 		editor.setText("ADD r0, r0, #1     ; r0 = r0 + r1\nADD r1, r0, #4     ; r0 = r0 + 4\nADD r0, r0, r0, LSL r0");
 		rightAccordion.setExpandedPane(registerPane);
 	}
-	
+
 	@FXML
 	public void step() {
 		logger.trace("Step");
 		program.step(registers);
 	}
-	
+
 	@SuppressWarnings({ "rawtypes", "unchecked", "unused" })
 	@FXML
 	public void startDebug() {
 		String sourceCode = editor.getText();
-		InputStream is = new ByteArrayInputStream(sourceCode.getBytes(StandardCharsets.UTF_8));
-		//InputStream is = ArmSourceTokenizerTest.class.getResourceAsStream("add.s");
+		InputStream is = new ByteArrayInputStream(
+				sourceCode.getBytes(StandardCharsets.UTF_8));
+		// InputStream is =
+		// ArmSourceTokenizerTest.class.getResourceAsStream("add.s");
 		program = new ArmSourceCompiler().compile(is);
-		
+
 		createRegisters();
-		
+
 		registersTable.setEditable(true);
-		
-		registers.addListener((MapChangeListener.Change<? extends String, ? extends Integer> change) -> {
-			registersTable.getItems().setAll(registers.entrySet());
-		});
-		
+
+		registers
+				.addListener((
+						MapChangeListener.Change<? extends String, ? extends Integer> change) -> {
+					registersTable.getItems().setAll(registers.entrySet());
+				});
+		registerNameCol.setComparator(new RegisterNameComparator());
 		registerNameCol.setCellValueFactory((p) -> {
-		    return new SimpleStringProperty(p.getValue().getKey());
+			return new SimpleStringProperty(p.getValue().getKey());
 		});
-		
+
 		registerNameCol.setCellFactory(TextFieldTableCell.forTableColumn());
 		registerNameCol.setEditable(false);
 		registerValueCol.setCellValueFactory((p) -> {
-		    return new ReadOnlyObjectWrapper<Integer>(p.getValue().getValue());
+			return new ReadOnlyObjectWrapper<Integer>(p.getValue().getValue());
 		});
-		
-		registerValueCol.setCellFactory(TextFieldTableCell.<Map.Entry<String,Integer>, Integer>forTableColumn(new IntegerStringConverter()));
 
-		registerValueCol.setOnEditCommit((TableColumn.CellEditEvent<Map.Entry<String,Integer>, Integer> t) -> {
-			registers.put(t.getTableView().getItems().get(t.getTablePosition().getRow()).getKey(),//key
-		           t.getNewValue());//val);
-		});
+		registerValueCol
+				.setCellFactory(TextFieldTableCell
+						.<Map.Entry<String, Integer>, Integer> forTableColumn(new IntegerStringConverter()));
+
+		registerValueCol
+				.setOnEditCommit((
+						TableColumn.CellEditEvent<Map.Entry<String, Integer>, Integer> t) -> {
+					registers.put(
+							t.getTableView().getItems()
+									.get(t.getTablePosition().getRow())
+									.getKey(),// key
+							t.getNewValue());// val);
+				});
 		registersTable.getItems().setAll(registers.entrySet());
-
 	}
-	
+
+	@FXML
+	public void loadFile() {
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Open ARM Source File");
+		File file = fileChooser.showOpenDialog(stage);
+		Charset encoding = FileUtilities.getSystemCharset();
+		String contents;
+		try {
+			contents = FileUtilities.readFile(file.getAbsolutePath(), encoding);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return;
+		} 
+		editor.setText(contents);
+	}
+
 	private void createRegisters() {
 		registers = FXCollections.observableHashMap();
 		registers.put("R0", 0);
@@ -110,5 +148,9 @@ public class Controller {
 		registers.put("R13", 0);
 		registers.put("R14", 0);
 		registers.put("R15", 0);
+	}
+
+	public void setStageAndSetupListeners(Stage stageIn) {
+		stage = stageIn;
 	}
 }
--- com.guygrigsby.jvarm.fxgui/src/com/guygrigsby/jvarm/fxgui/Main.java
+++ com.guygrigsby.jvarm.fxgui/src/com/guygrigsby/jvarm/fxgui/Main.java
@@ -2,6 +2,7 @@ package com.guygrigsby.jvarm.fxgui;
 	
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -11,8 +12,12 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			VBox root = (VBox)FXMLLoader.load(getClass().getResource("Main.fxml"));
-			Scene scene = new Scene(root,800,800);
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("Main.fxml"));
+			Parent root = (Parent)loader.load();
+			VBox vboxRoot = (VBox)root;
+			Scene scene = new Scene(vboxRoot,800,800);
+			Controller controller = (Controller)loader.getController();
+			controller.setStageAndSetupListeners(primaryStage);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setMaximized(true);
